d8fd9f2e66b31fc5a87ec6ec76f22b3eb7c8e8ad
fengyajing <fengyajing@kingsoft.com>
2022-07-20 12:31:45 +0800

    feat:[Exercise11]TCP

---
+++ CPPExercise/Exercise11TCP/NetConnectClient.cpp
@@ -0,0 +1,83 @@
+﻿#pragma comment(lib,"ws2_32.lib")
+#include <iostream>
+#include <winsock.h>
+#include <thread>
+
+#define PORT 8081
+#define SIZE 1024
+
+void SendBuffer(int InSockerID)
+{
+	char SendBuffer[SIZE] = { 0 };
+	while (true)
+	{
+		std::cout << "[ThreadID: " << std::this_thread::get_id() << "] Enter msg:\n";
+		std::cin >> SendBuffer;
+
+		send(InSockerID, SendBuffer, sizeof(SendBuffer), 0);
+		memset(SendBuffer, 0, sizeof(SendBuffer));
+	}
+}
+
+void RecvBuffer(int InSocketID)
+{
+	char ReceiveBuffer[SIZE] = { 0 };
+	while (true)
+	{
+		int ReadResult = recv(InSocketID, ReceiveBuffer, sizeof(ReceiveBuffer), 0);
+		if (ReadResult == -1)
+		{
+			std::cout << "Receive failed!";
+			return;
+		}
+		std::cout << "[ThreadID: " << std::this_thread::get_id() << "] Receive server msg : " << ReceiveBuffer << "\n";
+		memset(ReceiveBuffer, 0, sizeof(ReceiveBuffer));
+	}
+}
+
+int main()
+{
+	WORD WVersionRequested = MAKEWORD(1, 1);
+	WSADATA WsaDta;
+	int ErrorCode = WSAStartup(WVersionRequested, &WsaDta);
+
+	if (ErrorCode != 0)
+	{
+		std::cout << "Create socket failed!";
+		return 0;
+	}
+
+
+	int ClientSocketID = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (ClientSocketID == -1)
+	{
+		std::cout << "Create socket failed!";
+		return 0;
+	}
+
+	struct sockaddr_in ServerAddr;
+	memset(&ServerAddr, 0, sizeof(ServerAddr));
+
+	ServerAddr.sin_family = AF_INET;
+	ServerAddr.sin_port = htons(PORT);
+	ServerAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	int AddrLen = sizeof(ServerAddr);
+	int ConnectResult = connect(ClientSocketID, (struct sockaddr*)&ServerAddr, AddrLen);
+
+	if (ConnectResult == -1)
+	{
+		std::cout << "Connect failed!";
+		return 0;
+	}
+
+	std::cout << "Connect success!\n";
+
+	std::thread SendThread(SendBuffer, ClientSocketID);
+	std::thread ReceiveThread(RecvBuffer, ClientSocketID);
+
+	ReceiveThread.join();
+	SendThread.join();
+}
+
---
+++ CPPExercise/Exercise11TCP/NetConnectServer.cpp
@@ -0,0 +1,94 @@
+﻿#pragma comment(lib,"ws2_32.lib")
+#include <iostream>
+#include <winsock.h>
+#include <thread>
+
+
+#define PORT 8081
+#define SIZE 1024
+
+int WaitClient(int InSocketID)
+{
+    struct sockaddr_in CliendAddr;
+    int AddrLen = sizeof(CliendAddr);
+
+    std::cout << "Waiting for connection....\n";
+
+    int ClientSocketID = accept(InSocketID, (struct sockaddr*)&CliendAddr, &AddrLen);
+    if (ClientSocketID == -1)
+    {
+        std::cout << "Client connect failed!";
+        return -1;
+    }
+
+	std::cout << "Client connect success! SocketID:"<< ClientSocketID << "\n";
+
+    return ClientSocketID;
+}
+
+void ReceiveAndSendMessages(int InClientSocketID)
+{
+    char Buffer[SIZE];
+    while(true)
+    {
+        int ReadResult = recv(InClientSocketID, Buffer, sizeof(Buffer), 0);
+        if (ReadResult == -1)
+		{
+			std::cout << "Server recv failed!";
+			return;
+        }
+
+        std::cout << "Receive msg : " <<  Buffer << "\n";
+        send(InClientSocketID, Buffer, sizeof(Buffer), 0);
+    }
+}
+
+
+int main()
+{
+    WORD WVersionRequested = MAKEWORD(1, 1);
+    WSADATA WsaDta;
+    int ErrorCode = WSAStartup(WVersionRequested, &WsaDta);
+
+    if (ErrorCode != 0)
+    {
+		std::cout << "Create socket failed!";
+		return 0;
+    }
+
+    int ServerSocketID = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (ServerSocketID == -1)
+    {
+        std::cout << "Create socket failed!";
+        return 0;
+    }
+
+    struct sockaddr_in ServerAddr;
+    memset(&ServerAddr, 0, sizeof(ServerAddr));
+
+    ServerAddr.sin_family = AF_INET;
+    ServerAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    ServerAddr.sin_port = htons(PORT);
+
+    int BindResult = bind(ServerSocketID, (struct sockaddr*) &ServerAddr, sizeof(ServerAddr));
+
+    if (BindResult == -1)
+    {
+        std::cout << "Bind failed!";
+        return 0;
+    }
+
+    int ListenResult = listen(ServerSocketID, 5);
+    if (ListenResult == -1)
+    {
+		std::cout << "Listen failed!";
+		return 0;
+    }
+
+    int ClientSocketID = WaitClient(ServerSocketID);
+
+    std::thread ReceiveAndSend(ReceiveAndSendMessages, ClientSocketID);
+    ReceiveAndSend.join();
+
+}
